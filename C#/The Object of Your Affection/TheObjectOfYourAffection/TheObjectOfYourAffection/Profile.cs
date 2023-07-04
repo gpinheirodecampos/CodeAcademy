aa9cee481ab4568866e82b58ff6237a207b8612a
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2023-07-04 18:16:56 -0300

    Novo Projeto

---
+++ C#/The Object of Your Affection/TheObjectOfYourAffection/TheObjectOfYourAffection/Profile.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheObjectOfYourAffection
+{
+    class Profile
+    {
+        // Campos
+        private string nome;
+        private int idade;
+        private string cidade;
+        private string pais;
+        private string pronome;
+        private string[] hobbies;
+
+        // Propriedades
+        private string Nome { get; set; }
+        private int Idade
+        {
+            get { return idade; }
+            set
+            {
+                if (value > 18)
+                {
+                    idade = value;
+                }
+                else
+                {
+                    idade = 0;
+                }
+            }
+        }
+        private string Cidade { get; set; }
+        private string Pais { get; set; }
+        private string Pronome { get; set; }
+        private string[] Hobbies { get; set; }
+
+
+        // Construtores
+        public Profile(
+            string nome,
+            int idade,
+            string cidade,
+            string pais,
+            string pronome)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.Cidade = cidade;
+            this.Pais = pais;
+            this.Pronome = pronome;
+            this.Hobbies = new string[0];
+        }
+
+        public Profile(string nome, int idade) : this(nome, idade, "Unknown", "Unknown", "Unknown") { }
+
+
+        // Metodos
+        public string VerPerfil()
+        {
+            string bio = $"Nome: {Nome}\nIdade: {Idade}\nCidade: {Cidade}\nPais: {Pais}\nPronome:{Pronome}\n";
+
+            if (Hobbies.Length > 0)
+            {
+                bio += "\nHobbies:\n";
+
+                foreach (string hobby in Hobbies)
+                {
+                    bio += $"- {hobby}\n";
+                }
+            }
+
+            return bio;
+        }
+
+        public void SetHobbie(string[] hobbies)
+        {
+            this.Hobbies = hobbies;
+        }
+    }
+}
---
+++ C#/The Object of Your Affection/TheObjectOfYourAffection/TheObjectOfYourAffection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TheObjectOfYourAffection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando classe
+            Profile calleri = new Profile("Jonathan Calleri", 29, "Buenos Aires", "Argentina", "ele/dele");
+
+            // Adicionando hobbies
+            calleri.SetHobbie(new string[] {
+            "Brigar com os defensores adversarios",
+            "Fazer gols em classicos",
+            "Brigar com Abel Ferreira",
+            "Derrubar celulares dos torcedores de outros times"
+            });
+
+            // Exibindo perfil
+            Console.WriteLine(calleri.VerPerfil());
+
+            //
+        }
+    }
+}
